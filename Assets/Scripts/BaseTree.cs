a06fb2529e4b2cc59ec478c3eaa0fe96bf525269
BrandonBomar <156110257+BrandonBomar@users.noreply.github.com>
2024-01-24 10:57:41 -0600

    Project Developing (Third Commit)
    - Added Title Screen and Return Button
    - Coded BaseTree Script
    - Implemented Sound Effects

--- Assets/Scripts/BaseTree.cs
+++ Assets/Scripts/BaseTree.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BaseTree : MonoBehaviour
 {
+    string targetTag = "Tree";
+    string replaceTag = "Dead";
+
+    public AudioClip wav_Tree;
+    public int maxHealth = 25;
+    private int currentHealth;
+    public GameObject healthBar;
+    public Slider healthSlider;
+    public TMP_Text cHealth;
+    public int damageAmount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMouseEnter()
     {
-        
+        if (CompareTag(targetTag))
+        {
+            healthBar.SetActive(true);
+        }
+        if (!CompareTag(targetTag))
+        {
+            healthBar.SetActive(false);
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        if (CompareTag(targetTag))
+        {
+            healthBar.SetActive(false);
+        }
+    }
+
+    public virtual void OnMouseDown()
+    {
+        if (CompareTag(targetTag))
+        {
+            MakeSoundAndParticles();
+            TakeDamage(damageAmount);
+
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+        cHealth.SetText(currentHealth.ToString());
+    }
+
+    public virtual void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        SetHealth(currentHealth);
+    }
+
+    public virtual void MakeSoundAndParticles()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = wav_Tree;
+        audioSource.Play();
+    }
+
+    public virtual void Die()
+    {
+       // Add switch instance between models
+    }
+
+    public virtual void Revive()
+    {
+        currentHealth = maxHealth;
     }
 }
